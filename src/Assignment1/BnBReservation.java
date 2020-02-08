c0500ffb4007ee73d5212207b9b67b6408f661f5
Oliver Stappas <o.stappas@gmail.com>
2020-02-08 15:38:24 -0500

    Latest Changes2

--- src/Assignment1/BnBReservation.java
+++ src/Assignment1/BnBReservation.java
@@ -3,7 +3,6 @@ package Assignment1;
 public class BnBReservation extends HotelReservation {
     public BnBReservation(String reservationName, Hotel hotel, String inputRoomType, int numberOfNights) {
         super(reservationName, hotel, inputRoomType, numberOfNights);
-        HotelReservation hotelReservation = new HotelReservation(reservationName, hotel, inputRoomType, numberOfNights);
     }
 
     public int getCost() {
